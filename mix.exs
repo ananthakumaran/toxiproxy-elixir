c550dcf5c1af455d4aee4ac7d4036f27975fce94
Anantha Kumaran <ananthakumaran@gmail.com>
2017-02-13 21:51:05 +0530

    update mix.exs

--- mix.exs
+++ mix.exs
@@ -1,12 +1,17 @@
 defmodule Toxiproxy.Mixfile do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [app: :toxiproxy,
-     version: "0.1.0",
+     version: @version,
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     description: "Toxiproxy client",
+     package: package(),
+     docs: docs(),
      deps: deps()]
   end
 
@@ -19,7 +24,21 @@ defmodule Toxiproxy.Mixfile do
       {:tesla, "~> 0.6.0"},
       {:poison, ">= 1.0.0"},
       {:hackney, "1.6.5", only: :test},
-      {:mix_test_watch, "~> 0.3", only: :dev}
+      {:mix_test_watch, "~> 0.3", only: :dev},
+      {:ex_doc, "~> 0.13.1", only: :dev}
     ]
+  end
+
+  defp package do
+    %{licenses: ["MIT"],
+      links: %{"Github" => "https://github.com/ananthakumaran/toxiproxy-elixir"},
+      maintainers: ["ananthakumaran@gmail.com"]}
+  end
+
+  defp docs do
+    [source_url: "https://github.com/ananthakumaran/toxiproxy-elixir",
+     source_ref: "v#{@version}",
+     main: Toxiproxy,
+     extras: ["README.md"]]
   end
 end
